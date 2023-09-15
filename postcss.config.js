8d6e310fabcfe17105d85bd2aec829016aecf4b3
Gilbert Temgoua <gilbertemgoua@gmail.com>
2023-09-15 15:54:03 +0100

    fix tailwind not loading in prod

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: process.env.NODE_ENV === 'production'
+    ? {
+      tailwindcss: {},
+      autoprefixer: {},
+      cssnano: {
+        preset: 'default'
+      },
+      'postcss-preset-env': {},
+    }
+    : {
+      tailwindcss: {},
+      'postcss-preset-env': {}
+    }
 }
