6a48bc1d5e28c3ac5ee25ac049f190b0dd9d23db
Gilbert Temgoua <gilbertemgoua@gmail.com>
2023-09-15 15:30:31 +0100

    fix css not loading issue

--- postcss.config.ts
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	plugins:
-		process.env.NODE_ENV === 'production'
-			? ['tailwindcss', 'autoprefixer', ['cssnano', { preset: 'default' }], 'postcss-preset-env']
-			: ['tailwindcss', 'postcss-preset-env'],
-};
