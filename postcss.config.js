0a4f994a8a4b2a12e66bce48b3c804149667c2cc
Gilbert Temgoua <gilbertemgoua@gmail.com>
2023-09-15 16:23:57 +0100

    add cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   plugins:
-  // process.env.NODE_ENV === 'production'
-  //   ? 
-  {
-    tailwindcss: {},
-    autoprefixer: {},
-    cssnano: {
-      preset: 'default'
-    },
-    'postcss-preset-env': {},
-  }
-  // : {
-  //   tailwindcss: {},
-  //   'postcss-preset-env': {}
-  // }
+    process.env.NODE_ENV === 'production'
+      ?
+      {
+        tailwindcss: {},
+        autoprefixer: {},
+        cssnano: {
+          preset: 'default'
+        },
+        'postcss-preset-env': {},
+      }
+      : {
+        tailwindcss: {},
+        'postcss-preset-env': {}
+      }
 }
