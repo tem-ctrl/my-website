920d183a99226b5b2091122bb03db4d8737f261e
Gilbert Temgoua <gilbertemgoua@gmail.com>
2023-09-15 16:00:31 +0100

    update: remove env type condition

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,17 @@
 module.exports = {
-  plugins: process.env.NODE_ENV === 'production'
-    ? {
-      tailwindcss: {},
-      autoprefixer: {},
-      cssnano: {
-        preset: 'default'
-      },
-      'postcss-preset-env': {},
-    }
-    : {
-      tailwindcss: {},
-      'postcss-preset-env': {}
-    }
+  plugins:
+  // process.env.NODE_ENV === 'production'
+  //   ? 
+  {
+    tailwindcss: {},
+    autoprefixer: {},
+    cssnano: {
+      preset: 'default'
+    },
+    'postcss-preset-env': {},
+  }
+  // : {
+  //   tailwindcss: {},
+  //   'postcss-preset-env': {}
+  // }
 }
