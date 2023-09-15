79e845e8b6f2bee7d326dfacdf22faaf32c969a4
Gilbert Temgoua <gilbertemgoua@gmail.com>
2023-09-15 23:17:50 +0100

    update file

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,6 +1,7 @@
 module.exports = {
 	plugins:
-		process.env.NODE_ENV !== 'development'
-			? ['tailwindcss', 'autoprefixer', ['cssnano', { preset: 'default' }], 'postcss-preset-env']
-			: ['tailwindcss', 'postcss-preset-env'],
+		// process.env.NODE_ENV !== 'development'
+		// 	?
+		['tailwindcss', 'autoprefixer', ['cssnano', { preset: 'default' }], 'postcss-preset-env'],
+	// : ['tailwindcss', 'postcss-preset-env'],
 };
