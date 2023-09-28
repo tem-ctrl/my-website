92d03539219cb0e2caed1f723e87c869471cc410
Gilbert Temgoua <gilbertemgoua@gmail.com>
2023-09-29 00:22:16 +0100

    replace legacy google analytics by google tag

--- app/components/layout/GoogleAnalytics.tsx
+++ app/components/layout/GoogleAnalytics.tsx
@@ -29,17 +29,18 @@ const GoogleAnalytics = () => {
 					style={{ display: 'none', visibility: 'hidden' }}
 				/>
 			</noscript>
+			<Script async src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`} />
 			<Script
 				id="gtm-script"
 				strategy="afterInteractive"
 				dangerouslySetInnerHTML={{
 					__html: `
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer', '${GTM_ID}');
-  `,
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', ${GTM_ID});
+          `,
 				}}
 			/>
 		</>
