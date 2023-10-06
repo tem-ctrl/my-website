0587030ab8499e7f5840e6db0e21c9e29b768f1c
tem-ctrl <gilbertemgoua@gmail.com>
2023-10-06 11:43:10 +0100

    add ripple effect on buttons click

--- app/components/common/PrimaryButton.tsx
+++ app/components/common/PrimaryButton.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { ReactNode, ButtonHTMLAttributes, forwardRef } from 'react';
+import Ripples from 'react-ripples'
 
 type Mybutton = ButtonHTMLAttributes<HTMLButtonElement>;
 
@@ -14,14 +15,16 @@ const PrimaryButton = forwardRef<HTMLButtonElement, PrimaryButtonProps>((props, 
 	const { className, icon, text, ...restProps } = props;
 
 	return (
-		<button
-			className={`${className} smm:text-sm text-white font-medium rounded-full h-10 px-3 md:px-5 flex justify-center items-center gap-1.5 md:gap-3 bg-primary/90 hover:bg-primary`}
-			ref={ref}
-			{...restProps}
-		>
-			<span>{icon}</span>
-			<span>{text}</span>
-		</button>
+    <Ripples during={750} color='rgba(0,0,0,.4)' className='rounded-full'>
+      <button
+        className={`${className} smm:text-sm text-white font-medium rounded-full h-10 px-3 md:px-5 flex justify-center items-center gap-1.5 md:gap-3 bg-primary/90 hover:bg-primary`}
+        ref={ref}
+        {...restProps}
+      >
+        <span>{icon}</span>
+        <span>{text}</span>
+      </button>
+    </Ripples>
 	);
 });
 
--- app/components/common/PrimaryButtonLink.tsx
+++ app/components/common/PrimaryButtonLink.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import React, { ReactNode, AnchorHTMLAttributes, forwardRef } from 'react';
 import Link from 'next/link';
 import { MdOpenInNew } from 'react-icons/md';
+import Ripples from 'react-ripples'
 
 type MyButtonLink = AnchorHTMLAttributes<HTMLAnchorElement>;
 
@@ -17,19 +20,21 @@ const PrimaryButtonLink = forwardRef<HTMLAnchorElement, PrimaryButtonLinkProps>(
 	const blank = newTab && href !== '/';
 
 	return (
-		<Link
-			className={`${className} smm:text-sm text-white font-medium rounded-full h-10 px-3 md:px-5 flex justify-center items-center gap-1.5 md:gap-3 bg-primary/90 hover:bg-primary`}
-			ref={ref}
-			href={href}
-			{...restProps}
-			target={blank ? '_blank' : '_self'}
-		>
-			{!!icon && <span>{icon}</span>}
-			<span>
-				{text}
-				{blank && <MdOpenInNew className="ml-1 inline" />}
-			</span>
-		</Link>
+    <Ripples during={1000} color='rgba(255,255,255,.4)' className='rounded-full'>
+      <Link
+        className={`${className} smm:text-sm text-white font-medium rounded-full h-10 px-3 md:px-5 flex justify-center items-center gap-1.5 md:gap-3 bg-primary/90 hover:bg-primary`}
+        ref={ref}
+        href={href}
+        {...restProps}
+        target={blank ? '_blank' : '_self'}
+      >
+        {!!icon && <span>{icon}</span>}
+        <span>
+          {text}
+          {blank && <MdOpenInNew className="ml-1 inline" />}
+        </span>
+      </Link>
+    </Ripples>
 	);
 });
 
