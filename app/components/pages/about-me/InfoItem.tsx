b48851bea319bc62c16ef3f862f7a391e3df3b2a
tem-ctrl <gilbertemgoua@gmail.com>
2024-01-20 10:49:58 +0100

    remove daysjs

--- app/components/pages/about-me/InfoItem.tsx
+++ app/components/pages/about-me/InfoItem.tsx
@@ -1,25 +1,34 @@
 'use client'
 
-import React, { FC, useEffect } from 'react';
+import React, { FC, useCallback } from 'react';
 import { InfoItemType } from '@/app/utils/types';
 import Link from 'next/link';
 import { useLocale } from 'next-intl';
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
-import 'dayjs/locale/en';
-import 'dayjs/locale/fr';
 
 const InfoItem: FC<InfoItemType> = ({ className = '', label, value, isExperience = false }) => {
 	const isEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value);
-  const locale = useLocale()
+  const locale = useLocale();
 
-  dayjs.extend(relativeTime);
-  const exp = dayjs('2023-04-01').fromNow(true);
+  const exp = useCallback((): string => {
+    const start = new Date('2023-04-01');
+    const today = new Date();
+    const isEn = locale.includes('en');
 
-  useEffect(() => {
-    if (!isExperience) return;
-    console.info('WORK EXPERIENCE: ', exp);
-  }, [locale])
+    const m1 = today.getMonth() - start.getMonth()
+    + (12 * (today.getFullYear() - start.getFullYear()));
+
+    const m = today.getDate() > 15 ? m1 + 1 : m1;
+    const y = Math.floor(m / 12);
+    const m2 = m % 12;
+
+    const mths = isEn ? `month${m2 > 1 ? 's' : ''}` : 'mois';
+    const yrs = isEn ? `year${y > 1 ? 's' : ''}` : `an${y > 1 ? 's' : ''}`;
+    
+    if (y === 0) return `${m2} ${mths}`;
+    if (m2 === 0) return `${y} ${yrs}`;
+
+    return `${y} ${yrs} ${m2} ${mths}`;
+  }, [locale]);
 
   return (
 		<p className={`${className} flex gap-5`}>
@@ -29,7 +38,7 @@ const InfoItem: FC<InfoItemType> = ({ className = '', label, value, isExperience
 					{value}
 				</Link>
 			) : (
-				<span className="italic text-primary">{isExperience ? exp : value}</span>
+				<span className="italic text-primary">{isExperience ? exp() : value}</span>
 			)}
 		</p>
 	);
